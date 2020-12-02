9ea7527b7af7d3c5136fb054f995c4b5adf78a30
91-julian-sanchez <91.julian.sanchez@gmail.com>
2020-12-02 00:48:28 -0500

    Crear dataset de scrapper

--- main.py
+++ main.py
@@ -1,13 +1,61 @@
 import argparse
 import logging
+import datetime
+import csv
+import re
 import page_objects as pages
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
 from common import config
+
 logging.basicConfig(level=logging.INFO)
 # logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+is_well_formed_link = re.compile(r'^https?://.+/.+$')
+is_root_path = re.compile(r'^/.+$')
 
 
-def marketplaceScrapper(marketplace_uid,country_uid):
+def _build_link(host, link):
+    if is_well_formed_link.match(link):
+        return link
+    else:
+        return f"{host}/{link}"
+
+
+def _fetchProduct(marketplace, host, link):
+    logger.info(f'Start fetching product at {link}')
+
+    product = None
+    try:
+        product = pages.HomePage(marketplace, _build_link(host, link))
+        # print(product._html)
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning('Error while fetching article!', exc_info=False)
+        pass
+
+    if product and not product._html.body:
+        logger.warning('Invalid product. There is not body')
+        return None
+
+    return product._html
+
+
+def _save_products(marketplace_uid, country_uid, products):
+    # print("_save_products", products)
+    now = datetime.datetime.now().strftime('%Y_%m_%d')
+    out_file_name = f'./output/{marketplace_uid}_{country_uid}_{now}_products.csv'
+    csv_headers = list(products[0].keys())
+
+    with open(out_file_name, mode='w+') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for product in products:
+            writer.writerow(list(product.values()))
+    pass
+
+
+def marketplaceScrapper(marketplace_uid, country_uid):
     """
     Scrappy start function
     :param marketplace_uid: marketplace id for scrappy
@@ -17,7 +65,7 @@ def marketplaceScrapper(marketplace_uid,country_uid):
 
     subcategory = input("Escriba subcategoria que quiere buscar en deportes: ")
 
-    categoryPage = pages.CategoryPage(marketplace_uid, host, subcategory=subcategory, country_id=country_uid)
+    categoryPage = pages.CategorySectionPage(marketplace_uid, host, subcategory=subcategory, country_id=country_uid)
 
     counter = 1
     subcategories_links = categoryPage.subcategories_links
@@ -27,7 +75,7 @@ def marketplaceScrapper(marketplace_uid,country_uid):
         logger.info(f"{counter}. {link}")
         counter += 1
 
-    productsPage = pages.ProductPage(marketplace_uid, host, subcategory=subcategory, country_id=country_uid)
+    productsPage = pages.ProductSectionPage(marketplace_uid, host, subcategory=subcategory, country_id=country_uid)
     products = productsPage.produtcs
 
     print(f" \n\nPRODUCTOS({len(products)}):\n")
@@ -43,7 +91,17 @@ def marketplaceScrapper(marketplace_uid,country_uid):
         Promocionado (Ads): {product['promotional']}
         link: {product['link']}
         """)
+
+        # product_page = _fetchProduct(marketplace_uid, host, product['link'])
+        #
+        # if product_page:
+        #     print("Product scrappy!")
+        #     print(product_page.title)
+        #
+        #     break;
         counter += 1
+
+    _save_products(marketplace_uid, country_uid, products)
 
 
 if __name__ == '__main__':
