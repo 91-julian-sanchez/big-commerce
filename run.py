37f69de203e250935f57bbb5b88045627d43b683
91-julian-sanchez <91.julian.sanchez@gmail.com>
2021-03-20 22:56:18 -0500

    correr scrapy desde run.py

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 from scraper_motor.scraper_motor.spiders.category_glossary import CategoryGlossarySpider
 
 if __name__ == "__main__":
-  process = CrawlerProcess()
+  process = CrawlerProcess(get_project_settings())
   process.crawl(CategoryGlossarySpider)
   process.start()
